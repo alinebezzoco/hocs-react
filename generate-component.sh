34443c870e279989c743811f8ec72daa0274b090
Aline Bezzoco <contato.alinebezzoco@gmail.com>
2018-12-10 18:25:09 -0200

    bash command

---
+++ generate-component.sh
@@ -0,0 +1,12 @@
+#!/ How to run the command 
+#!/ In your terminal write bash generate-component.sh and press ENTER key
+echo Creating your component... 
+
+#!/ How to create a file
+#!/ Include your PATH file. Example: /src/components/Filename.js 
+#!/ You can create one or more files
+# echo "import React, { Component } from 'react';" > src/components/Home.js 
+echo -n > src/components/Footer.js 
+
+#!/ That's all folks!
+echo YAY! Have a nice day
